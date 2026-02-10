98d99d02521339c03e078b3054c3dfc265c82896
Priyansh Kumar <priyanshkumar0077@gmail.com>
2026-02-11 01:07:25 +0530

    added Transactional annotation

--- src/main/java/net/PORC/journalApp/service/JournalEntryService.java
+++ src/main/java/net/PORC/journalApp/service/JournalEntryService.java
@@ -6,6 +6,7 @@ import net.PORC.journalApp.entity.User;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,6 +19,7 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void SaveEntry(JournalEntry journalEntry, String username) {
         User user = userService.FindByUsername(username);
         journalEntry.setDate(LocalDateTime.now());
