6fd621cd07f4aaee17d3870e85048cd7f0cb45e2
Priyansh Kumar <priyanshkumar0077@gmail.com>
2026-03-03 16:10:17 +0530

    commit temporary

--- src/main/java/net/PORC/journalApp/configuration/SpringSecurity.java
+++ src/main/java/net/PORC/journalApp/configuration/SpringSecurity.java
@@ -22,16 +22,36 @@ public class SpringSecurity {
     @Autowired
     private CustomUserImpl customUser;
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/journal/**","/User/**")
-                        .authenticated().requestMatchers("/admin/**").hasRole("ADMIN")
-                        .anyRequest().permitAll())
-                .httpBasic(Customizer.withDefaults());
-        return http.build();
 
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+
+                        // Swagger public
+                       // .requestMatchers(
+                          //      "/v3/api-docs/**",
+                       //         "/swagger-ui/**",
+                         //       "/swagger-ui.html"
+                        //).permitAll()
+
+                        // Public endpoints (if any)
+                        .requestMatchers("/public/**").permitAll()
+
+                        // Admin protected
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+
+                        // Journal & user must login
+                        .requestMatchers("/journal/**", "/user/**").authenticated()
+
+                        // Everything else must login
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
     }
 
     @Bean
